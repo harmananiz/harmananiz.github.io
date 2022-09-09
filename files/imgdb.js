34da2d4a65a47558b9e8a79080f39696f1719839
harmananiz <85728876+harmananiz@users.noreply.github.com>
2022-09-09 12:15:40 -0400

    Add files via upload

---
+++ files/imgdb.js
@@ -0,0 +1,27 @@
+import reflabo from "../img/SKIN.jpg";
+import reflogo from "../img/reflet-logo.jpg";
+import face from "../img/face.jpg";
+import meetprofs from "../img/meet-the-prof.jpg";
+import meuf from "../img/meuf.jpg";
+import cure from "../img/run-for-the-cure.jpg";
+import iccitwrap from "../img/ES5 copy.jpg";
+import uiuxspeak from "../img/introducing-speakers-s2 copy.jpg";
+import sad2 from "../img/sedmen.jpg";
+import foodwri from "../img/food-wri.jpg";
+import iccithire from "../img/associate-hiring-s1.jpg";
+
+const db = [
+    {"name": "Reflet Skincare", "path": reflabo, "tags": ["product design"], },
+    {"name": "Reflet Logo", "path": reflogo, "tags": ["logos"], },
+    {"name": "Meet the Profs.", "path": meetprofs, "tags": ["graphics"], },
+    {"name": "CIBC Run for the Cure", "path": cure, "tags": ["graphics"], },
+    {"name": "ICCIT Food Writing", "path": foodwri, "tags": ["graphics"], },
+    {"name": "UI/UX Speaker Series", "path": uiuxspeak, "tags": ["graphics"], },
+    {"name": "Sadness, I ", "path": face, "tags": ["illustration", "graphics"], },
+    {"name": "Sadness, II", "path": sad2, "tags": ["illustration", "graphics"], },
+    {"name": "Woman, I", "path": meuf, "tags": ["illustration", "graphics"], },
+    {"name": "ICCIT Hiring", "path": iccithire, "tags": ["graphics"], },
+    {"name": "ICCIT Council Goodbye", "path": iccitwrap, "tags": ["graphics"], },
+];
+
+export default db;
